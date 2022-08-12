43ad9d98fbc18d1729a1b6585a3e78787a0ae755
johnBWeiss <yoniw7@gmail.com>
2022-08-12 21:32:18 +0300

    new background and sidebar responsiveness

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './pages/Home/Home';
-// import background from './assets/images/backgroundSCIFI.jpg'
+import background from './assets/images/backgroundSCIFI.jpg'
 import backgroundVid from './assets/images/backgroundVid.mp4'
 
 
@@ -11,13 +11,13 @@ const App = () => {
 
   return (
     <div className="app">
-      {/* <img
+      <img
         className='background'
         src={background}
         alt='background'
-      /> */}
-      <video src={backgroundVid} className='background'
-        autoPlay loop playsInline muted type='video/mp4' ></video>
+      />
+      {/* <video src={backgroundVid} className='background'
+        autoPlay loop playsInline muted type='video/mp4' poster={background}></video> */}
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
