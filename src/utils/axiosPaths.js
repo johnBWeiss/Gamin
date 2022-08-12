b446ca9fcbb8e42497218c6526f16ecee6a8166d
johnBWeiss <yoniw7@gmail.com>
2022-08-12 12:34:26 +0300

    sidebar logos basic structure

--- src/utils/axiosPaths.js
+++ src/utils/axiosPaths.js
@@ -8,15 +8,6 @@ export const getAllGamesOptions = {
     }
 };
 
-export const chosenGame = {
-    method: 'GET',
-    url: 'https://free-to-play-games-database.p.rapidapi.com/api/game',
-    params: { id: '' },
-    headers: {
-        'X-RapidAPI-Key': '09fd63799amsh2a9669f12442c07p113242jsn8e195afa1988',
-        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-    }
-};
 
 
 export const gameByCategory = {
