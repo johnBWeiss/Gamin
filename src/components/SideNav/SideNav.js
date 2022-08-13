595a077f9a1eb967908b83dd10c53a090c7fe5e8
johnBWeiss <yoniw7@gmail.com>
2022-08-13 13:19:22 +0300

    fixed no return bug in navgate home

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -4,15 +4,18 @@ import logos from '../../assets/logos/logoController'
 import { gameByCategory, getAllGamesOptions } from '../../utils/axiosPaths';
 import { changeOptions } from '../../store/gameSlice';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router'
+
 
 
 export const SideNav = (resetReff) => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const { resetReffHandler } = resetReff
     const { shooter, racing, strategy, sports, zombie, martialArts, home, controller } = logos
     const logoArray = [home, controller, shooter, strategy, racing, sports, zombie, martialArts]
 
-    const dispatch = useDispatch()
 
 
 
@@ -20,24 +23,22 @@ export const SideNav = (resetReff) => {
         resetReffHandler()
 
         if (v.title === 'home') {
+            navigate('/', { replace: true })
             return
+
         }
         if (v.title === 'All') {
             dispatch(changeOptions(getAllGamesOptions))
-
         }
-
         else {
             dispatch(changeOptions({ ...gameByCategory, params: { category: v.title } }))
         }
     }
     return (
 
-        // <div className='SideNavContainerBackground'>
         <div className='SideNavContainer'>
             <div className='SideNavInnerContainer'>
                 {logoArray.map((v, i) => (
-
                     <img
                         onClick={() => { dispatchCategoryHandler(v); resetReff() }}
                         key={v.title}
@@ -47,17 +48,8 @@ export const SideNav = (resetReff) => {
                         className='SideNavLogo'
                     />
                 ))}
-
-
-
-
-
-
-
-
             </div>
         </div>
-        // </div>
     )
 }
 
